6821ef91881c956902ec7e7b2e6d2e7f050092fb
Devika Sharma <devika.sharma95@gmail.com>
2019-08-31 23:56:36 +0530

    Add files via upload

---
+++ plot3.R
@@ -0,0 +1,12 @@
+unzip("exdata_data_household_power_consumption.zip")
+householdFile <- "household_power_consumption.txt"
+plotData <- read.table(householdFile, header=T, sep=";", na.strings="?")
+#Set time Variable
+finalData <- plotData[plotData$Date %in% c("1/2/2007","2/2/2007"),]
+SetTime <-strptime(paste(finalData$Date, finalData$Time, sep=" "),"%d/%m/%Y %H:%M:%S")
+finalData <- cbind(SetTime, finalData)
+#Plot 3
+plot(finalData$SetTime, finalData$Sub_metering_1, type="l", col=columnlines[1], xlab="", ylab="Energy sub metering")
+lines(finalData$SetTime, finalData$Sub_metering_2, col=columnlines[2])
+lines(finalData$SetTime, finalData$Sub_metering_3, col=columnlines[3])
+legend("topright", legend=labels, col=columnlines, lty="solid")
